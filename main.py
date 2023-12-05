ee6b774b8e7c33bbd923fbc01ed364937241631e
Arseniy <92452529+MCtop4ik@users.noreply.github.com>
2023-12-06 00:48:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,115 @@
-from dataclasses import dataclassfrom random import randintfrom typing import List@dataclassclass Cell:    asset_link: str    is_empty: bool@dataclass(frozen=True)class Room:    matrix: List[List[Cell]]class MapGenerator:    def __init__(self):        self.start_room = Room([[Cell('Start ', True)]])        self.portal_room = Room([[Cell('Portal', True)]])        self.enemy_room = Room([[Cell('Enemy ', True)]])        self.treasure_room = Room([[Cell('Treas ', True)]])        self.rooms_amount = 10        self.__min_enemies_rooms = 2        self.__max_enemies_rooms = 5        self.__max_treasuries_rooms = 3        self.__rooms = self.__create_rooms_matrix()        self.__coordinates = [(self.rooms_amount + 1, self.rooms_amount + 1)]    def __create_rooms_matrix(self):        rooms = [[None for _ in range(self.rooms_amount * 2 + 1)] for _ in range(self.rooms_amount * 2 + 1)]        rooms[self.rooms_amount + 1][self.rooms_amount + 1] = self.start_room        return rooms    def __create_room(self, amount, room):        x_gen, y_gen = self.__coordinates[-1]        for _ in range(amount):            alias_move = [-1, 1][randint(0, 1)]            direction = ['X', 'Y'][randint(0, 1)]            while self.__rooms[                x_gen + alias_move if direction == 'X' else x_gen            ][y_gen + alias_move if direction == 'Y' else y_gen] is not None:                alias_move = [-1, 1][randint(0, 1)]                direction = ['X', 'Y'][randint(0, 1)]            if direction == 'X':                x_gen += alias_move            if direction == 'Y':                y_gen += alias_move            self.__coordinates.append((x_gen, y_gen))            self.__rooms[x_gen][y_gen] = room    def __create_enemy_rooms(self):        enemies_rooms_number = randint(self.__min_enemies_rooms, self.__max_enemies_rooms)        self.__create_room(enemies_rooms_number, self.enemy_room)    def __create_portal_room(self):        self.__create_room(1, self.portal_room)    def __create_treasure_rooms(self):        cnt_treasuries = 0        left_shift = 1        right_shift = 1        chance = 3        iters_for_chance = 3        for enemy_room_coordinate in self.__coordinates[left_shift:-right_shift]:            x_enemy_room, y_enemy_room = enemy_room_coordinate            for _ in range(iters_for_chance):                if cnt_treasuries >= self.__max_treasuries_rooms:                    return                alias_move = [-1, 1][randint(0, 1)]                direction = ['X', 'Y'][randint(0, 1)]                if self.__rooms[                    x_enemy_room + alias_move if direction == 'X' else x_enemy_room                ][y_enemy_room + alias_move if direction == 'Y' else y_enemy_room] is None:                    if randint(0, chance) == randint(0, chance):                        if direction == 'X':                            x_enemy_room += alias_move                        if direction == 'Y':                            y_enemy_room += alias_move                        self.__coordinates.append((x_enemy_room, y_enemy_room))                        self.__rooms[x_enemy_room][y_enemy_room] = self.treasure_room                        cnt_treasuries += 1    def __crop(self):        new_rooms = list()        for i in range(len(self.__rooms)):            if any(elem is not None for elem in self.__rooms[i]):                new_rooms.append(self.__rooms[i])        self.__rooms = new_rooms        new_rooms = list()        rooms_transposed = tuple(zip(*self.__rooms[::-1]))        for i in range(len(rooms_transposed)):            if any(elem is not None for elem in rooms_transposed[i]):                new_rooms.append(rooms_transposed[i])        for _ in range(3):            new_rooms = list(zip(*new_rooms[::-1]))        self.__rooms = new_rooms        min_x = min(self.__coordinates, key=lambda elem: elem[0])[0]        min_y = min(self.__coordinates, key=lambda elem: elem[1])[1]        self.__coordinates = list(map(            lambda coordinate: (coordinate[0] - min_x, coordinate[1] - min_y), self.__coordinates))    def generate(self):        self.__create_enemy_rooms()        self.__create_portal_room()        self.__create_treasure_rooms()        self.__crop()        return self.__rooms, self.__coordinatesx = MapGenerator()for el in x.generate()[0]:    print(*el)
+from dataclasses import dataclass
+from random import randint
+from typing import List
+
+
+@dataclass
+class Cell:
+    asset_link: str
+    is_empty: bool
+
+
+@dataclass(frozen=True)
+class Room:
+    matrix: List[List[Cell]]
+
+
+class MapGenerator:
+
+    def __init__(self):
+        self.start_room = Room([[Cell('Start ', True)]])
+        self.portal_room = Room([[Cell('Portal', True)]])
+        self.enemy_room = Room([[Cell('Enemy ', True)]])
+        self.treasure_room = Room([[Cell('Treas ', True)]])
+        self.rooms_amount = 10
+        self.__min_enemies_rooms = 2
+        self.__max_enemies_rooms = 5
+        self.__max_treasuries_rooms = 3
+        self.__rooms = self.__create_rooms_matrix()
+        self.__coordinates = [(self.rooms_amount + 1, self.rooms_amount + 1)]
+
+    def __create_rooms_matrix(self):
+        rooms = [[None for _ in range(self.rooms_amount * 2 + 1)] for _ in range(self.rooms_amount * 2 + 1)]
+        rooms[self.rooms_amount + 1][self.rooms_amount + 1] = self.start_room
+        return rooms
+
+    def __create_room(self, amount, room):
+        x_gen, y_gen = self.__coordinates[-1]
+        for _ in range(amount):
+            alias_move = [-1, 1][randint(0, 1)]
+            direction = ['X', 'Y'][randint(0, 1)]
+            while self.__rooms[
+                x_gen + alias_move if direction == 'X' else x_gen
+            ][y_gen + alias_move if direction == 'Y' else y_gen] is not None:
+                alias_move = [-1, 1][randint(0, 1)]
+                direction = ['X', 'Y'][randint(0, 1)]
+            if direction == 'X':
+                x_gen += alias_move
+            if direction == 'Y':
+                y_gen += alias_move
+            self.__coordinates.append((x_gen, y_gen))
+            self.__rooms[x_gen][y_gen] = room
+
+    def __create_enemy_rooms(self):
+        enemies_rooms_number = randint(self.__min_enemies_rooms, self.__max_enemies_rooms)
+        self.__create_room(enemies_rooms_number, self.enemy_room)
+
+    def __create_portal_room(self):
+        self.__create_room(1, self.portal_room)
+
+    def __create_treasure_rooms(self):
+        cnt_treasuries = 0
+        left_shift = 1
+        right_shift = 1
+        chance = 3
+        iters_for_chance = 3
+
+        for enemy_room_coordinate in self.__coordinates[left_shift:-right_shift]:
+            x_enemy_room, y_enemy_room = enemy_room_coordinate
+            for _ in range(iters_for_chance):
+                if cnt_treasuries >= self.__max_treasuries_rooms:
+                    return
+                alias_move = [-1, 1][randint(0, 1)]
+                direction = ['X', 'Y'][randint(0, 1)]
+                if self.__rooms[
+                    x_enemy_room + alias_move if direction == 'X' else x_enemy_room
+                ][y_enemy_room + alias_move if direction == 'Y' else y_enemy_room] is None:
+                    if randint(0, chance) == randint(0, chance):
+                        if direction == 'X':
+                            x_enemy_room += alias_move
+                        if direction == 'Y':
+                            y_enemy_room += alias_move
+                        self.__coordinates.append((x_enemy_room, y_enemy_room))
+                        self.__rooms[x_enemy_room][y_enemy_room] = self.treasure_room
+                        cnt_treasuries += 1
+
+    def __crop(self):
+        new_rooms = list()
+        for i in range(len(self.__rooms)):
+            if any(elem is not None for elem in self.__rooms[i]):
+                new_rooms.append(self.__rooms[i])
+        self.__rooms = new_rooms
+        new_rooms = list()
+        rooms_transposed = tuple(zip(*self.__rooms[::-1]))
+        for i in range(len(rooms_transposed)):
+            if any(elem is not None for elem in rooms_transposed[i]):
+                new_rooms.append(rooms_transposed[i])
+        for _ in range(3):
+            new_rooms = list(zip(*new_rooms[::-1]))
+        self.__rooms = new_rooms
+        min_x = min(self.__coordinates, key=lambda elem: elem[0])[0]
+        min_y = min(self.__coordinates, key=lambda elem: elem[1])[1]
+        self.__coordinates = list(map(
+            lambda coordinate: (coordinate[0] - min_x, coordinate[1] - min_y), self.__coordinates))
+
+    def generate(self):
+        self.__create_enemy_rooms()
+        self.__create_portal_room()
+        self.__create_treasure_rooms()
+        self.__crop()
+        return self.__rooms, self.__coordinates
+
+
+x = MapGenerator()
+for el in x.generate()[0]:
+    print(*el)
