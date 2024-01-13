bc9a611145c1a65b15c2b6e1d6c552ea84e48677
Zetni <azakharov@workmovr.com>
2024-01-13 19:39:50 +0400

    movement after the player

--- sprites/enemy.py
+++ sprites/enemy.py
@@ -18,50 +18,45 @@ class Enemy(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()
         self.speed = 15
         self.angle = 0
-        self.offset_time_ms = 1000
+        self.offset_shoot_time_ms = 1000
         self.last_shoot_time = 0
+        self.last_moved_time = 0
+        self.offset_moved_time_ms = 100
+
+    def sign(self, x):
+        if x == 0:
+            return 0
+        if x > 0:
+            return 1
+        return -1
 
     def get_direction(self):
         x_player, y_player = SpriteGroups().player.get_player_coordinates()
 
-        relative_x = -(self.rect.centerx - x_player)
-        relative_y = -(self.rect.centery - y_player)
+        relative_x = x_player - self.rect.centerx
+        relative_y = y_player - self.rect.centery
 
         self.angle = atan2(relative_y, relative_x)
-
-        # if sqrt(x_player ** 2 + y_player ** 2) - \
-        #         sqrt(self.rect.centerx ** 2 + self.rect.centery ** 2) < Constants().fire_radius * sqrt(2):
-        #     pass
-
-        # if keys[pygame.K_UP] or keys[pygame.K_w]:
-        #     self.direction.y = -1
-        # elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        #     self.direction.y = 1
-        # else:
-        #     self.direction.y = 0
-        #
-        # if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        #     self.direction.x = 1
-        # elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        #     self.direction.x = -1
-        # else:
-        #     self.direction.x = 0
+        self.direction.x = self.sign(relative_x)
+        self.direction.y = self.sign(relative_y)
 
     def to_radians(self):
         return self.angle * 180 / pi
 
     def fire(self):
         x_player, y_player = SpriteGroups().player.get_player_coordinates()
-        if (pygame.time.get_ticks() - self.last_shoot_time > self.offset_time_ms * random.randint(1, 10)) and sqrt(
-                (x_player - self.rect.centerx) ** 2 + (
-                        y_player - self.rect.centery) ** 2) < Constants().fire_radius * sqrt(2):
+        if ((pygame.time.get_ticks() - self.last_shoot_time > self.offset_shoot_time_ms * random.randint(1, 10))
+                and sqrt((x_player - self.rect.centerx) ** 2 + (
+                        y_player - self.rect.centery) ** 2) < Constants().fire_radius * sqrt(2)):
             Bullet(SpriteGroups().bullets_group, self.to_radians(), 50, 60, self.pos, 'chest')
             self.last_shoot_time = pygame.time.get_ticks()
 
     def update(self):
         self.get_direction()
         self.fire()
-        self.rect.center += self.direction * self.speed
+        if pygame.time.get_ticks() - self.last_moved_time > self.offset_moved_time_ms * random.randint(1, 10):
+            self.rect.center += self.direction * self.speed
+            self.last_moved_time = pygame.time.get_ticks()
 
         while pygame.sprite.spritecollideany(self, SpriteGroups().walls_group):
             self.rect.center -= self.direction
