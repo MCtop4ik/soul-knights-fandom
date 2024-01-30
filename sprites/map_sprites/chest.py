786c018009147fd9e8658690c92e7cb62d5f728c
Белеко Никита <belnek.it.os@gmail.com>
2024-01-30 19:17:43 +0400

    Bugs, many bugs

--- sprites/map_sprites/chest.py
+++ sprites/map_sprites/chest.py
@@ -13,12 +13,14 @@ class Chest(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = Assets().images['chest']
         self.rect = self.image.get_rect(center=pos)
+        self.opened = False
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if pygame.sprite.spritecollideany(self, SpriteGroups().camera_group):
+        if pygame.sprite.spritecollideany(self, SpriteGroups().camera_group) and not self.opened:
             if keys[pygame.K_RETURN]:
+                print("chest")
                 InventoryV2().add_item_in_inventory(WeaponsList().weapons_list[random.randint(0, len(WeaponsList().weapons_list) - 1)])
-
+                self.opened = True
 
 
